ce35e6904e7ea37fca6c8085fc665e7cd0f47fd9
marciodavid <marciodavid81@gmail.com>
2025-09-07 11:24:01 -0300

    style: update background image URL in homepage component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -165,7 +165,7 @@ export default function HomePage() {
           className="absolute inset-0 bg-cover bg-center bg-fixed"
           style={{
             backgroundImage:
-              "url(https://images.unsplash.com/photo-1574323347407-f5e1ad6d020b?ixlib=rb-4.0.3&auto=format&fit=crop&w=2000&q=80)",
+              "url(https://images.unsplash.com/photo-1600747476236-76579658b1b1?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
           }}
         />
         <div className="absolute inset-0 bg-gradient-to-r from-green-900/80 via-green-800/70 to-green-700/60" />
